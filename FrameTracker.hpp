acd81e425ae8f9e535fb3b2c0439a8a211a39d2a
Yu Lu <yulu8798@gmail.com>
2014-10-27 20:30:47 +0800

    add two threshold

--- FrameTracker.hpp
+++ FrameTracker.hpp
@@ -17,20 +17,25 @@ class FrameTracker {
 			int spaceThreshold,
 			int lowThres,
 			int highThres,
-			Ptr<FeatureDetector> detector = FeatureDetector::create("ORB"),
+			ORB detector = ORB(100),
 			Ptr<DescriptorExtractor> extractor = DescriptorExtractor::create("ORB"),
 			Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create("BruteForce-Hamming")
 			);
 			
 			bool focus(Mat& frame);
 			void getGray(const Mat& frame, Mat& gray_frame);
+			
+			int numFeature;
 	
 	private:
+			void checkDistance(vector<KeyPoint>& keypoint);
+	
 			vector<vector<DMatch> >		m_matches;
 			
-			Ptr<FeatureDetector> 		m_detector;
+			ORB					 		m_detector;
 			Ptr<DescriptorExtractor>	m_extractor;
 			Ptr<DescriptorMatcher>		m_matcher;
+			vector<KeyPoint>			m_keypoints;
 			
 			int spaceT;
 			int lowT;
@@ -43,7 +48,7 @@ FrameTracker::FrameTracker  (
 			int spaceThreshold,
 			int lowThres,
 			int highThres,
-			Ptr<FeatureDetector> detector, 
+			ORB detector, 
 			Ptr<DescriptorExtractor> extractor,
 			Ptr<DescriptorMatcher> matcher
 							) : spaceT(spaceThreshold), lowT(lowThres), highT(highThres), m_detector(detector), m_extractor(extractor), m_matcher(matcher)
@@ -61,13 +66,16 @@ bool FrameTracker::focus(Mat& image)
 	Mat				 desc;
 	Mat				 frame;
 	
+	numFeature = 0;
+	
 	getGray(image, frame);
 	
 	//if first frame
 	if(init) {
 		init = false;
 				
-		m_detector->detect(frame, keypoint);
+		//m_detector.detect(frame, keypoint);
+		m_detector(frame, Mat(), keypoint, desc);
 		if(keypoint.empty())
 			return false;
 
@@ -82,13 +90,16 @@ bool FrameTracker::focus(Mat& image)
 		m_matcher->add(descriptors);
 
 		m_matcher->train();
+		
+		m_keypoints = keypoint;
 			
 		good = false;
 		
 	//not first frame
 	} else {
 	
-		m_detector->detect(frame, keypoint);
+		//m_detector.detect(frame, keypoint);
+		m_detector(frame, Mat(), keypoint, desc);
 		if(keypoint.empty())
 			return false;
 
@@ -98,11 +109,17 @@ bool FrameTracker::focus(Mat& image)
 		
 		m_matcher->radiusMatch(desc, m_matches, (float)spaceT);
 		
+		//numFeature = m_matches.size();
+		
+		checkDistance(keypoint);
+		
 		//check results
-		if (m_matches.size() > highT)
+		if ( numFeature > highT)
 			good = true;
-		if (m_matches.size() < lowT)
+		if ( numFeature < lowT)
 			good = false;	
+			
+
 		
 		//update matcher
 		m_matcher->clear();
@@ -113,7 +130,8 @@ bool FrameTracker::focus(Mat& image)
 		
 		m_matcher->train();	
 		
-		cout<<"valid desc: "<<m_matches.size()<<endl;
+		m_keypoints = keypoint;
+		
 	}
 	
 	return good;
@@ -132,6 +150,24 @@ void FrameTracker::getGray(const cv::Mat& image, cv::Mat& gray)
 	else if(image.channels() == 1)
 	{
 		gray = image;
+	}
+}
+
+void FrameTracker::checkDistance(vector<KeyPoint>& keypoint){
+	int size = m_matches.size();
+	numFeature = 0;
+	for(int i = 0; i < size; i++) {
+		vector<DMatch> bestMatch = m_matches[i];
+		if(bestMatch.size() > 0) {	
+			int qid = bestMatch[0].queryIdx;
+			int tid = bestMatch[0].trainIdx;
+			
+			int x = keypoint[qid].pt.x - m_keypoints[tid].pt.x;
+			int y = keypoint[qid].pt.y - m_keypoints[tid].pt.y;
+			
+			if(x * x + y * y < 12*12)
+				numFeature++;
+		}
 	}
 }
 	
--- main.cpp
+++ main.cpp
@@ -3,39 +3,55 @@
 #include <opencv2/features2d/features2d.hpp>
 #include <vector>
 #include <iostream>
+#include <fstream>
 
 #include "FrameTracker.hpp"
 
-using namespace cv;
-using namespace std;
+using namespace cv;   
+using namespace std; 
 
 int main(int argc, char** argv)
 { 
-	//read video
-	VideoCapture capture(0);
-	FrameTracker frameTracker(10, 20, 40);
-	
+		
 	Mat currentFrame; 
 	bool shouldQuit;
 	char* window="result";
+	ofstream myfile;
+	myfile.open ("./result.csv");
+	int low = 10;
+	int high = 20;
+	
+	//read video
+	VideoCapture capture("./test1.mp4");
+	FrameTracker frameTracker(20, low, high);
+
 	
 	namedWindow(window, CV_WINDOW_AUTOSIZE);
 	//detect keyframe
 	do
-	{
+	{  
 		capture >> currentFrame; 
 		if(currentFrame.empty())
 		{
 			shouldQuit = true;
-			continue;
+			continue; 
 		}
-		
+		 
 		bool f = frameTracker.focus(currentFrame);
+		
+		int b = 0;
+		if(f)
+			b = 100;
+			
+		myfile<<low<<"\t"<<high<<"\t"<<frameTracker.numFeature<<"\t"<<b<<endl;
+
 		
 		imshow(window, currentFrame);
 		waitKey(27);
 		
 	}while(!shouldQuit);
+	
+	myfile.close();
 	
 }
 
